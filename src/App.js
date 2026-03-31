fcc1db5f98ad022683aeb13efa5adc4188ad5863
roelbonagua-lang <roelbonagua@gmail.com>
2026-03-31 12:43:40 +0800

    resolved merge conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'; 
 import './App.css';
+import Row from './row';
+import request from './request';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey bonagua-netflix-clone! Let's build a Netflix clone!</h1>
+      <h1>Hey bonagua-netflix-clone! Let's build a Netflix Clone Front-end today</h1>
+      <Row  title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals } />
+      <Row  title="TRENDING" fetchUrl={request.fetchTrending } />
+
+      <Row  title="TOP RATED" fetchUrl={request.fetchTopRated } />
+      <Row  title="ACTION MOVIES" fetchUrl={request.fetchActionMovies } />
+      <Row  title="COMEDY MOVIES" fetchUrl={request.fetchComedyMovies } />
+      <Row  title="HORROR MOVIES" fetchUrl={request.fetchHorrorMovies } />
+      <Row  title="ROMANCE MOVIES" fetchUrl={request.fetchRomanceMovies } />
+      <Row  title="DOCUMENTARIES" fetchUrl={request.fetchDocumentaries } />
     </div>
   );
 }
 
 export default App;
+
---
+++ src/row.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+
+        fetchData(); 
+
+    }, [fetchUrl]);
+
+    console.log(movies);
+
+    return (
+        <div className="row">
+            <h2>{title}</h2>
+            <div className="row__posters">
+                {/* several row__poster elements */}
+                {movies.map(movie => (
+                    <img
+                        key={movie.id}
+                        className="row__poster"
+                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                        alt={movie.title || movie.name}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Row;
